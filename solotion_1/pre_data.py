4d3f4b7d748aa319576fb025201a96421352f32c
royal-dargon <2312749625@qq.com>
2022-10-28 01:45:46 +0800

    feat:add the run code about text

--- solotion_1/pre_data.py
+++ solotion_1/pre_data.py
@@ -2,16 +2,21 @@
 import os
 
 from PIL import Image
-from transformers import BertTokenizer, BertModel
+from transformers import BertTokenizer
 import torch
 import torch.nn as nn
 import torchvision
 from torchvision import transforms
+import numpy as np
+import torch.optim as optim
+
+import model
 
 
 bert_en_model = "../pre_model/pretrained_berts/bert_en"
 tokenizer = BertTokenizer.from_pretrained(bert_en_model)
-model = BertModel.from_pretrained(bert_en_model)
+
+label_name = ['neutral', 'negative', 'positive']
 
 
 def get_single():
@@ -58,8 +63,52 @@ def get_single():
     return data_rows
 
 
+def text2id(rows):
+    input_id = tokenizer.encode(
+        rows,
+        add_special_tokens=True,
+        max_length=100,
+        padding='max_length',
+        return_tensors='pt'
+    )
+    mask = [1 if t != 0 else 0 for t in input_id[0, :].tolist()]
+    mask = torch.tensor(mask).reshape([1, -1])
+    text_id = [input_id, mask]
+    return text_id
+
+
+def label2features(label):
+    label_id = torch.tensor([label_name.index(label)], dtype=torch.long)
+    return label_id
+
+
 if __name__ == "__main__":
     # 获取文件夹的源数据
     data = get_single()
     # 将这些row的数据送入BERT与res-net进行特征的提取
+    # text = text2id(data["text"])
+    epoch = 5
+    TextModel = model.Text2Features()
+    text_optim = optim.Adam([param for param in TextModel.parameters()], lr=1e-5)
+    loss_func_text = nn.CrossEntropyLoss()
+    # 文本模型的测试
+    for i in range(epoch):
+        # 注意一下标签的序号
+        j = 0
+        for row in data["text"][:100]:
+            text_optim.zero_grad()
+            text = text2id(row)
+            out = TextModel(text)
+            out = torch.unsqueeze(out, dim=0)
+            text_label = data["text_labels"][j]
+            j += 1
+            y = label2features(text_label)
+            loss = loss_func_text(out, y)
+            loss.backward()
+            text_optim.step()
+            if j % 10 == 0:
+                print(loss, out)
+
+
+
 
