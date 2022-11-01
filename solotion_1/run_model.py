89d0a16eead350f2824225de3122b1374596d972
royal-dargon <2312749625@qq.com>
2022-11-01 16:43:33 +0800

    feat:add the acc and polish up the loss

--- solotion_1/run_model.py
+++ solotion_1/run_model.py
@@ -4,41 +4,62 @@ from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-from transformers import logging
 
 
 import model
 import pre_data
-from utils import cpu, device, config
-
-logging.set_verbosity_warning()
+from utils import cpu, device, config, compute_acc
 
 
 def train_textmodel(train_loader, val_loader, text_model, optimizer, loss_func):
     """train the text model"""
     tr_losses = []
     val_losses = []
+    val_acc = []
     for epoch in tqdm(range(config["text_model"]["epoch"]), desc="training the text model ..."):
         start = time.time()
-        j = 0
+        train_index, val_index = 0, 0
+        train_size, val_size = 0, 0
         losses = 0
         for row in tqdm(train_loader["text"]):
+            batch_size = len(row)
+            train_size += batch_size
             optimizer.zero_grad()
             text = pre_data.text2id(row)
             out = text_model(text)
             out = out.to(cpu)
-            text_label = train_loader["text_label"][j]
-            j += 1
+            # print(out.shape)
+            text_label = train_loader["text_label"][train_index]
+            train_index += 1
             y = pre_data.label2features(text_label)
             y = y.squeeze(1)
+            # print(y.shape)
             loss = loss_func(out, y)
             loss.backward()
             optimizer.step()
-            losses += loss
-        tr_losses.append(losses / j)
+            losses += loss.item() * batch_size
+        tr_losses.append(losses / train_size)
         end = time.time()
-        print("the %d epoch train losses is %f the time is %f s" % (epoch + 1, losses / j, (end - start)))
+        print("the %d epoch train losses is %f the time is %f s" % (epoch + 1, losses / train_size, (end - start)))
+
+        losses = 0
+        with torch.no_grad():
+            for row in tqdm(val_loader["text"]):
+                batch_size = len(row)
+                val_size += batch_size
+                text = pre_data.text2id(row)
+                out = text_model(text)
+                out = out.to(cpu)
+                text_label = val_loader["text_label"][val_index]
+                val_index += 1
+                y = pre_data.label2features(text_label)
+                y = y.squeeze(1)
+                losses = loss_func(out, y)
+                acc_nums = compute_acc(out, y)
+                losses += loss.item() * batch_size
+            val_losses.append(losses / val_size)
+            val_acc.append(acc_nums/val_size)
+            print("the %d epoch val losses is %f the acc is %f" % (epoch + 1, losses/val_size, acc_nums/val_size))
 
     # 保存模型的参数
     torch.save(TextModel.state_dict(), './save/single/textmodel.pt')
@@ -63,7 +84,7 @@ if __name__ == "__main__":
     TextModel = model.Text2Features(device, use_gpu=is_use_gpu)
     if is_use_gpu:
         TextModel = TextModel.to(device)
-    text_optim = optim.Adam([param for param in TextModel.parameters()], lr=1e-5)
+    text_optim = optim.Adam([param for param in TextModel.parameters()], lr=1e-3)
     loss_func_text = nn.CrossEntropyLoss()
     # scheduler_text = ReduceLROnPlateau(text_optim, mode='min', patience=5, factor=0.1, verbose=True)
     train_textmodel(train_loader=res_data["train"], val_loader=res_data["val"],
--- solotion_1/utils.py
+++ solotion_1/utils.py
@@ -1,3 +1,4 @@
+# this is the file about config and utils
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -7,4 +8,16 @@ config = {
     "text_model": {}
 }
 
-config["text_model"]["epoch"] = 100
+config["text_model"]["epoch"] = 1
+
+
+def compute_acc(out, y):
+    acc_nums = 0
+    _, index = torch.topk(out, 1, dim=1, largest=True, sorted=True)
+    _, index_real = torch.topk(y, 1, dim=1, largest=True, sorted=True)
+    res_index = 0
+    for res in index:
+        if res == index_real[res_index]:
+            acc_nums += 1
+        res_index += 1
+    return acc_nums
